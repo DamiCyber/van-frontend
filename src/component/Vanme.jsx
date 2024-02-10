ce7bb7f6e173757d50c40e2cadf1318c98998c52
damola ifatifo <ifatifod4@gmail.com>
2024-02-10 16:37:05 +0100

    update

--- src/component/Vanme.jsx
+++ src/component/Vanme.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import "../assets/styles/vanme.css"
+import "../assets/styles/Vanme.css"
 const Vanme = () => {
   const url = 'https://vanlifeback.onrender.com/api/vans';
   const [data, setdata] = useState([]);
